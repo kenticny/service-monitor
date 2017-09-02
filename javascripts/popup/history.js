003f2072f1becd1f3ab6f4120bc41a2180baa25d
kenticny <kenticny@gmail.com>
2017-09-02 14:43:57 +0800

    Update history module

--- javascripts/popup/history.js
+++ javascripts/popup/history.js
@@ -1,7 +1,33 @@
-const History = {
-  id: Number,
+let historyInstance = null;
+class HistoryStack {
+  constructor() {
+    this.stack = [1];
+  }
+  static getInstance() {
+    if (!historyInstance) {
+      historyInstance = new HistoryStack();
+    }
+    return historyInstance;
+  }
+  pushHistory(history) {
+    this.stack.push(history);
+  }
+  popHistory() {
+    return this.stack.pop();
+  }
+  clearHistory() {
+    let firstView = this.stack.shift();
+    this.stack = [];
+    return firstView;
+  }
+  isRoot() {
+    return this.stack.length == 0;
+  }
 }
 
-function pushHistory(history) {
-  
+class History {
+  constructor(view, params) {
+    this.view = view;
+    this.params = params;
+  }
 }
