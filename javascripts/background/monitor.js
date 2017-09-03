6c7f990a1c920f9d74d8eac5449563d42a115547
kenticny <kenticny@gmail.com>
2017-09-03 23:50:23 +0800

    monitor feature

--- javascripts/background/monitor.js
+++ javascripts/background/monitor.js
@@ -1,5 +1,8 @@
 function addMonitor(monitor) {
   monitor = _.pick(monitor, _.keys(Monitor))
+  if (_.keys(monitor).length == 0) {
+    return Promise.reject('Missing parameters');
+  }
   return pushItem(monitor)
 }
 
